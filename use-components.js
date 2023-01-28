c106280566e2dd9faafa0fa3fc09a0d901d0bc70
UniBreakfast <unibreakfast1@gmail.com>
2023-01-28 21:21:21 +0200

    fill attributes too

--- use-components.js
+++ use-components.js
@@ -43,36 +43,63 @@ function placeComponents() {
   const placeholders = names.flatMap(name => [...document.getElementsByTagName(name)])
 
   for (const ph of placeholders) {
-    const nodes = components[ph.localName].map(part => part.cloneNode(true))
+    const clonedNodes = components[ph.localName].map(part => part.cloneNode(true))
+    const {elements, nodes} = getHungry(clonedNodes)
 
-    ph.replaceWith(...nodes)
+    ph.replaceWith(...clonedNodes)
 
-    for (const node of getHungryNodesOf(nodes)) fill(node).with(ph.dataset)
+    for (const el of elements) fill(el).attributesWith(ph.dataset)
+    for (const node of nodes) fill(node).textWith(ph.dataset)
   }
 
   if (placeholders.length) placeComponents()
 }
 
-function getHungryNodesOf(nodes) {
-  const hungryNodes = []
-  
-  nodes = nodes.flatMap(getAllNodes)
+function getHungry(clonedNodes) {
+  const allNodes = clonedNodes.flatMap(getAllNodes)
+  const elements = []
+  const nodes = []
 
-  for (const node of nodes) {
-    const [...matches] = node.nodeValue?.matchAll(hungryRE) || []
-    if (matches.length) hungryNodes.push({matches, node})
+  for (const node of allNodes) {
+    if (node.nodeType == 1) {
+      const attributes = []
+
+      for (const attr of node.attributes) {
+        const [...matches] = attr.value?.matchAll(hungryRE) || []
+        if (matches.length) attributes.push({matches, attr})
+      }
+
+      if (attributes.length) elements.push({attributes, node})
+    } 
+    else {
+      const [...matches] = node.nodeValue?.matchAll(hungryRE) || []
+      if (matches.length) nodes.push({matches, node})
+    }
   }
 
-  return hungryNodes
+  return {elements, nodes}
 }
 
-function fill({matches, node}) {
+function fill(item) {
   return {
-    with(dataset) {
+    attributesWith(dataset) {
+      const {attributes, node} = item
+
+      for (const {matches, attr} of attributes) {
+        for (const [substr, name] of matches) {
+          attr.value = attr.value.replaceAll(substr, dataset[name] || '')
+        }
+        
+        if (!attr.value) node.removeAttribute(attr.name)
+      }
+    },
+
+    textWith(dataset) {
+      const {matches, node} = item
       for (const [substr, name] of matches) {
         node.nodeValue = node.nodeValue.replaceAll(substr, dataset[name] || '')
       }
-    }
+    },
   }
 }
 
